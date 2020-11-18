6e40e96bd4311db2048a5027e7716ea30bfe0389
Chris C <christopher.cruzcosa@gmail.com>
2020-11-18 13:22:36 -0800

    fixed front page typo

--- src/pages/Front/index.js
+++ src/pages/Front/index.js
@@ -16,7 +16,7 @@ function Front() {
           <p className="text-right" >Welcome to this island I've carved out of the much wider sea.</p>
           <p className="text-right" >You're welcome to take a look around, before you set sail again.</p>
           <p className="text-right" >The sunsets are beautiful here.</p>
-          <p className="text-right" ><NavLink class="btn btn-outline-dark justify-content-right" to="/portfolio" target="_blank" rel="noopener noreferrer">View Projects <i class="icon-briefcase3"></i></NavLink></p>
+          <p className="text-right" ><NavLink class="btn btn-outline-dark justify-content-right" to="/portfolio">View Projects <i class="icon-briefcase3"></i></NavLink></p>
         </div>
       </div>
     </div>
